d94ab2de0270daf979ad882a167b55b0834f01dc
Denis Shayahmetov <Denis Shayahmetov>
2016-10-15 18:29:29 +0300

    Remove backup

--- backup/src/routes.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Route, IndexRoute } from 'react-router';
-import TaskTable from './components/TaskTable/TaskTable';
-
-export default (
-    <Route path="/">
-        <IndexRoute component={TaskTable} />
-    </Route>
-);
